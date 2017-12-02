f738e2275d7c13289e66ed7c75c8c783bab7f0dc
Julian Sandersius <30510599+JSandersius@users.noreply.github.com>
2017-12-02 03:44:32 -0700

    commitone

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+var timeLeft = 30;
+var elem = document.getElementById("timeRemainingBox");
+
+
+
+var timerId = setInterval(countdown, 1000);
+
+function countdown() {
+  if (timeLeft == 0) {
+    setTimeout(timerId);
+    doSomething();
+  } else {
+    elem.innerHTML = timeLeft + ' seconds remaining';
+    timeLeft--;
+
+
+  }
+}
